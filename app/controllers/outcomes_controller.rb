ebb7c61dbb4c06ce3c9b350dad5f936ec57c5f33
x <mfhansen@mit.edu>
2014-05-16 09:32:21 -0700

    Link between course and outcomes

--- app/controllers/outcomes_controller.rb
+++ app/controllers/outcomes_controller.rb
@@ -3,6 +3,11 @@ class OutcomesController < ApplicationController
   end
 
   def show
+    @courses = Course.find(params[:id])
+    @outcomes = Outcome.where(course_id: Course.find(params[:id]))
+
+    #@departments = Department.find(params[:id])
+    #@courses = Course.where(department_id: Department.find(params[:id]))
   end
 
   def new
