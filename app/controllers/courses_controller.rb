892baa524fea4ae74130e5c70199ce52b2a57416
Jon Daries <daries@mit.edu>
2014-05-17 16:54:06 -0400

    Many improvements and CSS

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -7,6 +7,14 @@ class CoursesController < ApplicationController
     @course = Course.find(params[:id])
     @department = @course.department
     @outcomes = Course.find(params[:id]).outcomes
+    @show_flags = []
+    @outcomes.each do |out|
+      if out.assessments.size != 0
+        @show_flags << true
+      else
+        @show_flags << false
+      end
+    end
   end
 
   def new
