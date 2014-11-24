489aa4b164218554f2aaa1c3e577ef7875364ac3
Jon Daries <daries@mit.edu>
2014-11-24 17:56:30 -0500

    Fixed bug from last commit, all tests pass and match new Roles department values

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,12 +7,10 @@ class HomeController < ApplicationController
     roles = Roles.for(@creds)
     #see if they have access
     render :no_access if roles.count == 0
-
     #lookup their departments
     @departments = roles.map do |dept|
       Department.find_by_role_department(dept)
     end
-
     redirect_to department_path(@departments.first) if @departments.count == 1
   end
 end
