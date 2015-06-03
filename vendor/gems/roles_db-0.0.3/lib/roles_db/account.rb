12897b4e7cab63d0166d640434ded65cf1135e2c
Derek Prior <derekprior@gmail.com>
2015-06-03 14:04:29 -0400

    Extract RolesDB to external repo
    This previously existed vendored in the app, which can lead to version
    safety issues and also makes it less discoverable. I believe the
    original code was written by Cory Foy, though I'm not certain and I
    can't track down any original repositories for it. The commit history
    for the vendored gem yielded no good details on this.

--- vendor/gems/roles_db-0.0.3/lib/roles_db/account.rb
+++
@@ -1,5 +0,0 @@
-module RolesDb
-  class Account
-    attr_accessor :authorization_id, :category, :effective_date, :expiration_date, :function, :is_active_now, :name, :account_has_access_to
-  end
-end
