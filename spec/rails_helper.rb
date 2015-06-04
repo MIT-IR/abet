dfeaabadc89baa0af22f8ee6f457ff7ec92ec725
Derek Prior <derekprior@gmail.com>
2015-06-04 14:59:31 -0400

    Update spec helpers
    This makes them more in line with what we typically use via suspenders.
    Most importantly, it randomizes the test order.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,8 +8,8 @@ Dir[Rails.root.join("spec/support/*.rb")].sort.each { |file| require file }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
+  config.infer_base_class_for_anonymous_controllers = false
   config.infer_spec_type_from_file_location!
   config.include FactoryGirl::Syntax::Methods
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,13 @@
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
   config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
     mocks.verify_partial_doubles = true
   end
+
+  config.order = :random
 end
