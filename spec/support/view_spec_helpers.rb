7443270b3f52364d7899cefd95f42735f50e79f1
Derek Prior <derekprior@gmail.com>
2015-06-04 22:50:29 -0400

    Don't show links to unauthorized actions
    We shouldn't' show the user links to actions we know they are not
    authorized to perform.
    
    In the process of implementing the view specs for this, I came across a
    tricky issue with regards to stubbing the `policy` method in the view
    (which is provided by pundit via a controller helper method). The
    `verify_partial_doubles` setting in rspec was raising an error because
    the view itself does not actually implement `policy`. Rails magic!
    
    I thought I could trick it by passing a lambda named `policy` to the
    view as a local variable but that did not work. Instead, I followed some
    workarounds found here: https://github.com/rspec/rspec-rails/issues/1076
    
    Essentially in each view spec, we extend the view object to contain the
    controller helper methods that we encounter in our views. Then, since
    they are now technically present on the view object, we can stub them.
    I'm not entirely comfortable with that solution since we're actually
    changing the object under test, but I thought it was the best option.
    The other things I considered were disabling partial double verification
    in view specs with an around filter or testing these with feature specs.

--- spec/support/view_spec_helpers.rb
+++ spec/support/view_spec_helpers.rb
@@ -2,8 +2,26 @@ module ViewSpecHelpers
   def page
     Capybara.string(rendered)
   end
+
+  def stub_policy(methods)
+    allow(view).to receive(:policy).and_return(double("Policy", methods))
+  end
+
+  def initialize_controller_helper_methods(view)
+    view.extend ControllerHelperMethods
+  end
+
+  module ControllerHelperMethods
+    def policy(object)
+      raise "stub policy behavior to test this view"
+    end
+  end
 end
 
 RSpec.configure do |config|
   config.include ViewSpecHelpers, type: :view
+
+  config.before(:each, type: :view) do
+    initialize_controller_helper_methods(view)
+  end
 end
---
+++ spec/views/courses/_outcomes.html.erb_spec.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+
+describe "courses/outcomes" do
+  it "shows the add assessment link if authorized" do
+    outcome = create(:outcome)
+    stub_policy(create_assessments?: true)
+
+    render "courses/outcomes", course: outcome.course
+
+    expect(page).to have_link("Add new assessment")
+  end
+
+  it "does not show the add assessment link if unauthorized" do
+    outcome = create(:outcome)
+    stub_policy(create_assessments?: false)
+
+    render "courses/outcomes", course: outcome.course
+
+    expect(page).not_to have_link("Add new assessment")
+  end
+
+  it "shows the create_custom outcomes link if authorized" do
+    course = build_stubbed(:course, has_custom_outcomes: true)
+    stub_policy(create_outcomes?: true)
+
+    render "courses/outcomes", course: course, unassociated_outcomes: []
+
+    expect(page).to have_link("New custom outcome")
+  end
+
+  it "does not show the create_custom outcomes link if unauthorized" do
+    course = build_stubbed(:course, has_custom_outcomes: true)
+    stub_policy(create_outcomes?: false)
+
+    render "courses/outcomes", course: course, unassociated_outcomes: []
+
+    expect(page).not_to have_link("New custom outcome")
+  end
+end
