b675210f9d4f9652e66f3cdf9693ee339d9807d5
Derek Prior <derekprior@gmail.com>
2015-06-15 10:10:17 -0400

    Adopt standard outcomes via outcomes dashboard
    This test used to go through the course show page, which will likely be
    going away as the admin section of the site is finished up today. The
    workflow for adopting custom outcomes should be triggered from the
    outcomes dashboard.

--- spec/features/admin_adopts_standard_outcomes_for_course_spec.rb
+++ spec/features/admin_adopts_standard_outcomes_for_course_spec.rb
@@ -1,12 +1,12 @@
 require "rails_helper"
 
-feature "User adopts standard outcomes for a course" do
-  scenario "successfully" do
+feature "Admin adopts standard outcomes for a course" do
+  scenario "en masse" do
     course = create(:course)
     standard_outcome = create(:standard_outcome)
     user = user_with_admin_access_to(course.department)
 
-    visit course_path(course, as: user)
+    visit outcomes_dashboard_path(course, as: user)
     click_on "Adopt Default Outcomes"
 
     expect(page).to have_content(standard_outcome.name)
