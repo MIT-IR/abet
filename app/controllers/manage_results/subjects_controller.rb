379795ceca1c102ec5680667c680bfd80c072b9e
Jon Daries <daries@mit.edu>
2015-09-21 15:28:19 -0400

    Trying to do the ordering in the view instead

--- app/controllers/manage_results/subjects_controller.rb
+++ app/controllers/manage_results/subjects_controller.rb
@@ -6,6 +6,5 @@ class ManageResults::SubjectsController < ApplicationController
   def show
     @subject = Subject.find(params[:id])
     authorize(@subject)
-    @assessments = @subject.direct_assessments(:order => 'name ASC')
   end
 end
