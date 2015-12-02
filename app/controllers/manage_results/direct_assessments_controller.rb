d37606d577dc4a33155bc4d4f1277b75b4dbd7e4
Derek Prior <derekprior@gmail.com>
2015-12-02 13:07:42 -0500

    Remove unused "edit" action
    There is no route that points to this controller action.

--- app/controllers/manage_results/direct_assessments_controller.rb
+++ app/controllers/manage_results/direct_assessments_controller.rb
@@ -4,11 +4,6 @@ class ManageResults::DirectAssessmentsController < ApplicationController
     authorize(@assessment)
   end
 
-  def edit
-    @assessment = DirectAssessment.find(params[:id])
-    authorize(@assessment)
-  end
-
   def update
     @assessment = DirectAssessment.find(params[:id])
     authorize(@assessment)
