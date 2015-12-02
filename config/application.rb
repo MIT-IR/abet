8f2a6ed79530637a039d059f362e560b0dde562c
Derek Prior <derekprior@gmail.com>
2015-12-02 10:40:00 -0500

    Update dependencies
    There were several outdated dependencies that were easily updated.

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ Bundler.require(*Rails.groups)
 
 module Abet
   class Application < Rails::Application
+    config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
     config.middleware.use Rack::Deflater
   end
