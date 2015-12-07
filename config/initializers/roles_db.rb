fc03a4571d6fd99e5fe74892bb610e4af7b17ac3
Derek Prior <derekprior@gmail.com>
2015-12-07 17:04:38 -0500

    Update RolesDB client
    The client itself now houses the WSDL. The client allows for overriding
    the endpoint url, which we will do in a later change. I want to deploy
    this change first to be sure things still work as usual with the new
    RolesDB client version.

--- config/initializers/roles_db.rb
+++ config/initializers/roles_db.rb
@@ -7,18 +7,15 @@ if Rails.env.development?
   ENV["eppn"] ||= "daries@mit.edu"
 end
 
-if Rails.env.test?
-  RolesDb.configure do |config|
-    config.mocked_account_list_file = Rails.root.join("config", "test-roles.yaml")
-    config.strategy_class = "RolesDb::LocalRoles"
-  end
-end
-
 RolesDb.configure do |config|
   config.ssl_cert_key_file = Rails.root.join("config", "certs", "outcomes-key.pem")
   config.ssl_cert_file = Rails.root.join("config", "certs", "outcomes.cer")
   config.ssl_ca_cert_file = Rails.root.join("config", "certs", "mit.pem")
-  config.roles_wsdl_file = Rails.root.join("config", "certs", "roles.wsdl")
   config.ssl_cert_key_password = "abetiscool"
   config.proxy_user_name = "abetuser"
+
+  if Rails.env.test?
+    config.mocked_account_list_file = Rails.root.join("config", "test-roles.yaml")
+    config.strategy_class = "RolesDb::LocalRoles"
+  end
 end
