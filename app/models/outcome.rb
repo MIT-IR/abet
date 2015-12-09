f74a1762368d4e0212d2d46e6921e1b6df5084ce
Derek Prior <derekprior@gmail.com>
2015-12-09 14:12:20 -0500

    Format associations
    Get rid of some long lines that were impacting readability

--- app/models/outcome.rb
+++ app/models/outcome.rb
@@ -1,12 +1,18 @@
 class Outcome < ActiveRecord::Base
   belongs_to :course, counter_cache: true
 
+  has_one :department, through: :course
   has_many :outcome_assessments, dependent: :destroy
-  has_many :direct_assessments, through: :outcome_assessments, source: :assessment, source_type: "DirectAssessment"
-  has_many :indirect_assessments, through: :outcome_assessments, source: :assessment, source_type: "IndirectAssessment"
   has_many :alignments
   has_many :standard_outcomes, through: :alignments
-  has_one :department, through: :course
+  has_many :direct_assessments,
+    through: :outcome_assessments,
+    source: :assessment,
+    source_type: "DirectAssessment"
+  has_many :indirect_assessments,
+    through: :outcome_assessments,
+    source: :assessment,
+    source_type: "IndirectAssessment"
 
   accepts_nested_attributes_for :alignments,
     reject_if: ->(attributes) { attributes[:level].blank? },
