ef14f5d390f33546833eea025d156fc2cc5d0351
Ali Aas <ali@thoughtbot.com>
2015-12-15 16:13:40 -0500

    Fixes to progress bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,13 @@
 module ApplicationHelper
   def progress_bar_percentage_width(amount, total)
-    amount = amount.to_f
-    total = total.to_f
-    width = (amount / total) * 100
+    if amount > 0
+      amount = amount.to_f
+      total = total.to_f
+      width = (amount / total) * 100
 
-    number_to_percentage(width, precision: 0)
+      number_to_percentage(width, precision: 0)
+    else
+      amount
+    end
   end
 end
