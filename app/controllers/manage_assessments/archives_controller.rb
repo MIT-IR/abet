62925719768816da1019182a1ce4ae0c2363b58c
Derek Prior <derekprior@gmail.com>
2015-12-21 11:42:32 -0500

    Allow users to undo an archive action
    Very similar to GMail, allow users to immediately undo an action they
    may have done in error.

--- app/controllers/manage_assessments/archives_controller.rb
+++ app/controllers/manage_assessments/archives_controller.rb
@@ -3,13 +3,22 @@ module ManageAssessments
     def create
       authorize(assessment, :update?)
       assessment.archive
-      redirect_to_back_or_default root_path, success: t(".success")
+
+      flash[:html_safe] = true
+      redirect_to_back_or_default(
+        root_path,
+        success: t(".success", name: assessment.name, undo: undo_link(assessment)),
+      )
     end
 
     def destroy
       authorize(assessment, :update?)
       assessment.unarchive
-      redirect_to_back_or_default root_path, success: t(".success")
+
+      redirect_to_back_or_default(
+        root_path,
+        success: t(".success", name: assessment.name),
+      )
     end
 
     private
@@ -24,6 +33,22 @@ module ManageAssessments
 
     def assessment_id
       params["#{assessment_type.model_name.singular}_id"]
+    end
+
+    def undo_link(assessment)
+      view_context.link_to(t(".undo"), undo_href(assessment), method: :delete)
+    end
+
+    def undo_href(assessment)
+      if assessment.is_a?(DirectAssessment)
+        manage_assessments_direct_assessment_archive_path(
+          direct_assessment_id: assessment.id,
+        )
+      else
+        manage_assessments_indirect_assessment_archive_path(
+          indirect_assessment_id: assessment.id,
+        )
+      end
     end
   end
 end
--- spec/features/user_archives_assessments_spec.rb
+++ spec/features/user_archives_assessments_spec.rb
@@ -9,17 +9,22 @@ feature "User archives assessments" do
     click_link "Archive"
 
     expect(page).to have_content "has been archived"
-    expect(page).not_to have_content assessment.name
+    expect(page).to have_content "No direct assessments"
   end
 
-  scenario "when viewing assessments for a course" do
-    assessment = create(:survey)
+  scenario "when viewing assessments can immediately undo the archive" do
+    assessment = create(:direct_assessment)
     user = user_with_assessments_access_to(assessment.outcomes.first.department)
 
     visit manage_assessments_course_assessments_path(assessment.courses.first, as: user)
     click_link "Archive"
 
     expect(page).to have_content "has been archived"
-    expect(page).not_to have_content assessment.name
+    expect(find("#direct_assessments")).not_to have_content assessment.name
+
+    click_link "Undo"
+
+    expect(page).to have_content "has been unarchived"
+    expect(find("#direct_assessments")).to have_content assessment.name
   end
 end
