864c168d52325cfbc71407ef3fdbc64f7de208f9
Derek Prior <derekprior@gmail.com>
2016-01-07 10:04:46 -0500

    Use fake roles on staging for now
    There's some more debugging that has to happen with the RoleDB on the
    staging server. For now, we can have it use the fake roles that we use
    in development. This will allow Jon to review things on staging (as his
    user is associated to all the fake permissions. Other users will still
    have no department access.
    
    This is meant to let us move forward with acceptance on staging.

--- config/initializers/roles_db.rb
+++ config/initializers/roles_db.rb
@@ -1,25 +1,22 @@
-if Rails.env.development?
-  RolesDb.configure do |config|
+RolesDb.configure do |config|
+  if Rails.env.development? || Rails.env.staging?
     config.mocked_account_list_file = Rails.root.join("config", "dev-roles.yaml")
     config.strategy_class = "RolesDb::LocalRoles"
+
+    ENV["eppn"] ||= "daries@mit.edu"
   end
-
-  ENV["eppn"] ||= "daries@mit.edu"
-end
-
-RolesDb.configure do |config|
-  config.ssl_cert_key_file = Rails.root.join("config", "certs", "outcomes-key.pem")
-  config.ssl_cert_file = Rails.root.join("config", "certs", "outcomes.cer")
-  config.ssl_ca_cert_file = Rails.root.join("config", "certs", "mit.pem")
-  config.ssl_cert_key_password = "abetiscool"
-  config.proxy_user_name = "abetuser"
 
   if Rails.env.test?
     config.mocked_account_list_file = Rails.root.join("config", "test-roles.yaml")
     config.strategy_class = "RolesDb::LocalRoles"
   end
 
-  if Rails.env.production? || Rails.env.staging?
+  if Rails.env.production?
+    config.ssl_cert_key_file = Rails.root.join("config", "certs", "outcomes-key.pem")
+    config.ssl_cert_file = Rails.root.join("config", "certs", "outcomes.cer")
+    config.ssl_ca_cert_file = Rails.root.join("config", "certs", "mit.pem")
+    config.ssl_cert_key_password = "abetiscool"
+    config.proxy_user_name = "abetuser"
     config.endpoint = "https://rolesws.mit.edu/rolesws/services/roles"
   end
 end
