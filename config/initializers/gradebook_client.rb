361527b60b4e4eca4704bec135380eefb96c0dee
Derek Prior <derekprior@gmail.com>
2016-01-07 14:32:32 -0500

    Configure GradebookClient for staging and prod

--- config/initializers/gradebook_client.rb
+++ config/initializers/gradebook_client.rb
@@ -2,10 +2,10 @@ GradebookClient.configure do |config|
   if Rails.env.production?
     config.adapter = GradebookClient::Adapters::LearningModules.new(
       endpoint: "https://learning-modules.mit.edu/service/gradebook/",
-      client_cert: File.read("config/certs/outcomes-cert.pem"),
+      client_cert: File.read("config/certs/outcomes.cer"),
       client_key: File.read("config/certs/outcomes-key.pem"),
     )
-  elsif Rails.env.development? && File.exists?("config/certs/outcomes-dev-cert.pem")
+  elsif Rails.env.staging? || File.exists?("config/certs/outcomes-dev-cert.pem")
     config.adapter = GradebookClient::Adapters::LearningModules.new(
       endpoint: "https://learning-modules-dev.mit.edu:8443/service/gradebook/",
       client_cert: File.read("config/certs/outcomes-dev-cert.pem"),
