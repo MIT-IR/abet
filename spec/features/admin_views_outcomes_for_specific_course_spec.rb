2aa828e9430a8f976bfe24bad877bcf42e1211ee
Margaret <margaretoti@users.noreply.github.com>
2017-05-10 16:09:58 -0400

    Add a custom outcome button appears every course
    When the user clicks the "Adopt Standard Outcomes" button from the
    "Manage Outcomes Dashboard" and then returns to the dashboard and clicks
    on "View Outcomes", an "Add a Custom Outcome" button should appear
    regardless of whether the course has unaligned or fully aligned
    outcomes.
    
    This branch contains partially refactored tests.

---
+++ spec/features/admin_views_outcomes_for_specific_course_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+feature 'Admin views the outcomes for a specific course' do
+  scenario 'with fully aligned outcomes' do
+    course = create(:course, :fully_aligned)
+    admin = user_with_admin_access_to(course.department)
+
+    view_outcomes_of_a_specific_course_as(admin)
+
+    course_outcome_name = course.outcomes.first.name
+    course_outcome_description = course.outcomes.first.description
+
+    expect(page).to have_content('Add A Custom Outcome')
+    expect(page).to have_outcome_name(course_outcome_name)
+    expect(page).to have_outcome_description(course_outcome_description)
+    expect(page).to have_content('Edit Outcome')
+  end
+
+  scenario 'with unaligned outcomes' do
+    course = create(:course, :with_unaligned_outcome)
+    admin = user_with_admin_access_to(course.department)
+
+    view_outcomes_of_a_specific_course_as(admin)
+
+    course_outcome_name = course.outcomes.first.name
+    course_outcome_description = course.outcomes.first.description
+
+    expect(page).to have_content('Add A Custom Outcome')
+    expect(page).to have_outcome_name(course_outcome_name)
+    expect(page).to have_outcome_description(course_outcome_description)
+    expect(page).to have_content('Edit Outcome')
+  end
+
+  def have_outcome_name(name)
+    have_content(name)
+  end
+
+  def have_outcome_description(text)
+    have_content(text)
+  end
+
+  def view_outcomes_of_a_specific_course_as(user)
+    visit manage_outcomes_root_path(as: user)
+    click_on 'View Outcomes'
+  end
+end
