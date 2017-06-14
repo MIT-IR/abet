31385990fa4bc3486e00151fb84b1d57fec856e7
Derek Prior <derekprior@gmail.com>
2017-06-14 14:41:57 -0400

    Remove unused helper method

--- app/helpers/outcomes_helper.rb
+++ app/helpers/outcomes_helper.rb
@@ -1,11 +1,4 @@
 module OutcomesHelper
-  def outcome_details(outcome)
-    content_tag :ul, class: "assessment-details" do
-      concat content_tag(:li, "Course: #{outcome.course}")
-      concat content_tag(:li, "Outcome: #{outcome}")
-    end
-  end
-
   def outcome_card_class(outcome)
     if outcome.outcome_coverages.present?
       "outcome-card-matched"
