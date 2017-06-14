4d14a1f2c8325011606a08c74fbd46c344376a7f
Derek Prior <derekprior@gmail.com>
2017-06-14 11:42:01 -0400

    Remove assessment-based associations from outcomes
    The UI has been updated to use assignment and coverage associations
    where we were previously relying on assessments. The assessment ->
    assignment migration does not need any of these associations on
    `Outcome` so they are safe to delete.
    
    The `assessments` association had a counter cache defined, but this is
    no longer necessary. We removed the corresponding test for this.

--- app/models/outcome.rb
+++ app/models/outcome.rb
@@ -4,14 +4,9 @@ class Outcome < ActiveRecord::Base
   belongs_to :course, counter_cache: true
 
   has_one :department, through: :course
-  has_many :outcome_assessments, dependent: :destroy
   has_many :outcome_coverages
   has_many :alignments
   has_many :standard_outcomes, through: :alignments
-  has_many :assessments, through: :outcome_assessments
-  has_many :active_assessments,
-    -> { where(archived: false) },
-    through: :outcome_assessments
 
   accepts_nested_attributes_for :alignments,
     reject_if: ->(attributes) { attributes[:level].blank? },
