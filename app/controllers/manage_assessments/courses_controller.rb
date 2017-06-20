89003264b2d34d5292f0d19a44329f14981b2629
Derek Prior <derekprior@gmail.com>
2017-06-20 11:18:54 -0400

    Remove unused `courses#index`
    Also cleaned up some remaining translations from previously deleted
    assessments views

--- app/controllers/manage_assessments/courses_controller.rb
+++ app/controllers/manage_assessments/courses_controller.rb
@@ -1,13 +1,7 @@
 class ManageAssessments::CoursesController < ApplicationController
-  skip_after_action :verify_authorized
-  after_action :verify_policy_scoped
-
-  def index
-    @courses = policy_scope(Course).includes(outcomes: :department)
-  end
-
   def show
     @course = CourseCoverage.new(course)
+    authorize(@course)
 
     if @course.has_coverages?
       render :show
