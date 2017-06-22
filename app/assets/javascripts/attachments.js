0ba33f7d9a651ed8e27bc791a73f945158f5b523
Margaret <margaretoti@users.noreply.github.com>
2017-06-22 14:10:57 -0400

    Remove choose file button from edit assignment
    This branch removes the choose file button present on the edit
    assignment page.
    
    https://trello.com/c/P0dYb3tH

--- app/assets/javascripts/attachments.js
+++ app/assets/javascripts/attachments.js
@@ -3,7 +3,7 @@ $(function() {
   var fileName = ".file-name";
 
   $(document).on("cocoon:before-insert", ".attachments", function(e, insertedItem) {
-    $(insertedItem).find(fileInput).hide().click();
+    $(insertedItem).find(fileInput).click();
 
     $(insertedItem).on("change", function(ev) {
       var selectedFileName = ev.target.files[0].name;
