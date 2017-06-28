ff700361e806b018c799ddc2a97fbaf481a42f06
Margaret <margaretoti@users.noreply.github.com>
2017-06-28 15:32:06 -0400

    Split tests

--- spec/features/user_deletes_attachment_from_courses_page_spec.rb
+++ spec/features/user_deletes_attachment_from_courses_page_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-feature "User views attachments related to a specific assignment", js: true do
+feature "User deletes attachments related to a specific assignment", js: true do
   scenario "successfully" do
     assignment = create(:assignment)
     attachment = create(:attachment, attachable: assignment)
@@ -8,10 +8,10 @@ feature "User views attachments related to a specific assignment", js: true do
 
     visit manage_assignments_course_path(assignment.course.id, as: user)
     click_on t("manage_assignments.outcome_coverages.outcome_coverage.attachments-expandable-link")
-
-    expect(page).to have_content(attachment.file_file_name)
-
-    click_on t("manage_assignments.outcome_coverages.outcome_coverage.attachments-expandable-link")
+    save_and_open_page
+    within("table.assignment-attachments") do
+      click_on t("manage_assignments.courses.attachments.delete-link")
+    end
 
     expect(page).to have_no_content(attachment.file_file_name)
   end
