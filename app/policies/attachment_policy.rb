fb66ba113347e09eaa58203f8556e7d967d19334
Margaret <margaretoti@users.noreply.github.com>
2017-06-28 14:29:16 -0400

    Green - User can delete attachment

--- app/policies/attachment_policy.rb
+++ app/policies/attachment_policy.rb
@@ -2,4 +2,8 @@ class AttachmentPolicy < ApplicationPolicy
   def show?
     user.manage_assignments?(record.course_department)
   end
+
+  def destroy?
+    show?
+  end 
 end
--- spec/features/user_views_attachments_from_courses_page_spec.rb
+++ spec/features/user_views_attachments_from_courses_page_spec.rb
@@ -10,4 +10,18 @@ feature "User views attachments related to a specific assignment" do
 
     expect(page).to have_content(attachment.file_file_name)
   end
+
+  scenario "deletes" do
+    assignment = create(:assignment)
+    attachment = create(:attachment, attachable: assignment)
+    user = user_with_assignments_access_to(assignment.course.department)
+
+    visit manage_assignments_course_path(assignment.course.id, as: user)
+    save_and_open_page
+    within("table.assignment-attachments") do
+      click_on "Delete"
+    end
+
+    expect(page).to have_no_content(attachment.file_file_name)
+  end
 end
