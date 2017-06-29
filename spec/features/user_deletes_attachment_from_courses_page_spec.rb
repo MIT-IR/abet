9686455bdf7b6a835325aa50dbc213929aafd6a9
Margaret <margaretoti@users.noreply.github.com>
2017-06-29 17:01:19 -0400

    User views attachment number
    This branch adds the number of attachments associated with an attachment
    next to the attachments link.

--- spec/features/user_deletes_attachment_from_courses_page_spec.rb
+++ spec/features/user_deletes_attachment_from_courses_page_spec.rb
@@ -1,12 +1,19 @@
 require "rails_helper"
 
-feature "User deletes attachments related to a specific assignment", js: true do
+feature "User views attachment count and deletes attachments related to a
+specific assignment", js: true do
   scenario "successfully" do
     assignment = create(:assignment)
     attachment = create(:attachment, attachable: assignment)
+    attachment_count = assignment.attachments.size
     user = user_with_assignments_access_to(assignment.course.department)
 
     visit manage_assignments_course_path(assignment.course.id, as: user)
+
+    within(".class-card-assignment-controls") do
+      expect(page).to have_content(attachment_count)
+    end
+
     click_on t("manage_assignments.outcome_coverages.outcome_coverage.attachments-expandable-link")
 
     within(".assignment-attachments") do
@@ -15,5 +22,9 @@ feature "User deletes attachments related to a specific assignment", js: true do
 
     expect(page).to have_content(assignment.name)
     expect(page).to have_no_content(attachment.file_file_name)
+
+    within(".class-card-assignment-controls") do
+      expect(page).to have_no_content(attachment_count)
+    end
   end
 end
